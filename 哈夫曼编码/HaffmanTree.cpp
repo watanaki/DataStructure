197c51d40e4db389cc93614cec6a1029aa327edf
watanaki <90881607+watanaki@users.noreply.github.com>
2022-11-15 13:50:42 +0800

    Add files via upload

---
+++ 哈夫曼编码/HaffmanTree.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include "HaffmanTree.h"
+using namespace std;
+
+int main()
+{
+	HuffmanTree A;
+	cout << "请输入要编码字符的个数：";
+	int num;cin >> num;
+	CreateHuffmanTree(A, num);
+	PrintTree(A, num);
+	chara *b=new chara[num];
+	Code(A,num,b);
+	for (int i = 0; i < num; i++)
+		cout << b[i] ;
+	Decode(A,num,"101100101010");
+	
+	/*char a[4] = "123";
+	a[3] = '\0';
+	char b[3];
+	strcpy(b, a + 2);
+	cout << b;*/
+	/*int a, b;
+	SeclectMin(A, 4, &a, &b);
+	cout << a << ' ' << b;
+	A[0].weight = 99; A[1].weight = 88;
+	A[3].weight = 17;
+	SeclectMin(A, 4, &a, &b);
+	cout << a << ' ' << b;*/
+}
+
